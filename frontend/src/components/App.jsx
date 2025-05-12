f0b5a82405f9b78d391fd388dec14cc7f810318e
HARINIKATHIRVEL <harinik587244@gmail.com>
2025-05-12 23:29:53 +0530

    Create App.jsx

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,7 @@
+import Welcome from './Welcome';
+import Homepage from './Homepage';
+
+<Routes>
+  <Route path="/" element={<Welcome />} />
+  <Route path="/home" element={<Homepage />} />
+</Routes>
